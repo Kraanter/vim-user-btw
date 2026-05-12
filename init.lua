68c785ed24e2b95f238ea781bb6c876630cbdc3d
Krijn Grimme <krijngrimme@hotmail.com>
2026-05-12 21:16:35 +0900

    Add opencode.nvim with direct pack-based keymap setup

--- init.lua
+++ init.lua
@@ -159,6 +159,7 @@ vim.pack.add({
     { src = "https://github.com/nvim-lua/plenary.nvim" },
     { src = "https://github.com/theprimeagen/harpoon",                   version = "harpoon2" },
     { src = "https://github.com/ThePrimeagen/99" },
+    { src = "https://github.com/nickjvandyke/opencode.nvim" },
     { src = "https://github.com/jackplus-xyz/monaspace.nvim" },
     { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
 })
@@ -306,6 +307,42 @@ cmp.setup({
 -- ============================================================================
 -- 99 setup (OpenCode + cmp completion)
 -- ============================================================================
+---@type opencode.Opts
+vim.g.opencode_opts = {}
+
+vim.o.autoread = true
+
+vim.keymap.set({ "n", "x" }, "<C-a>", function()
+    require("opencode").ask("@this: ", { submit = true })
+end, { desc = "Ask opencode..." })
+
+vim.keymap.set({ "n", "x" }, "<C-x>", function()
+    require("opencode").select()
+end, { desc = "Execute opencode action..." })
+
+vim.keymap.set({ "n", "t" }, "<C-.>", function()
+    require("opencode").toggle()
+end, { desc = "Toggle opencode" })
+
+vim.keymap.set({ "n", "x" }, "go", function()
+    return require("opencode").operator("@this ")
+end, { desc = "Add range to opencode", expr = true })
+
+vim.keymap.set("n", "goo", function()
+    return require("opencode").operator("@this ") .. "_"
+end, { desc = "Add line to opencode", expr = true })
+
+vim.keymap.set("n", "<S-C-u>", function()
+    require("opencode").command("session.half.page.up")
+end, { desc = "Scroll opencode up" })
+
+vim.keymap.set("n", "<S-C-d>", function()
+    require("opencode").command("session.half.page.down")
+end, { desc = "Scroll opencode down" })
+
+vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+
 local _99 = require("99")
 
 _99.setup({
